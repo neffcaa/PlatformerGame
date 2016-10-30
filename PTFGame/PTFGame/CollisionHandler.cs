96c1e33371d3286e61972a8c0826b9d02a34bdb7
Zach <zacharywright77@gmail.com>
2016-10-30 02:00:04 -0500

    changed collision handler class

---
+++ PTFGame/PTFGame/CollisionHandler.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace PTFGame
+{
+    public abstract class CollisionHandler
+    {
+        public ArrayList thingsOnMap;
+
+
+        public void checkCollisions()
+        {
+
+
+        }
+
+        public void handleCollisions()
+        {
+
+        }
+
+
+
+
+
+
+
+    }
+
+
+}
