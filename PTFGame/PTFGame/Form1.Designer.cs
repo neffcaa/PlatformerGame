afcc100a044d6a08267169f66c77473c60e144ec
Charlie <neffcaa@uni.edu>
2016-10-29 23:46:11 -0500

    Added Movement and Jumping

--- PTFGame/PTFGame/Form1.Designer.cs
+++ PTFGame/PTFGame/Form1.Designer.cs
@@ -29,11 +29,59 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.Screen = new System.Windows.Forms.Panel();
+            this.Player = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.Screen.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Player)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Screen
+            // 
+            this.Screen.Controls.Add(this.Player);
+            this.Screen.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Screen.Location = new System.Drawing.Point(0, 0);
+            this.Screen.Name = "Screen";
+            this.Screen.Size = new System.Drawing.Size(671, 544);
+            this.Screen.TabIndex = 0;
+            this.Screen.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // Player
+            // 
+            this.Player.BackColor = System.Drawing.Color.Black;
+            this.Player.Location = new System.Drawing.Point(310, 196);
+            this.Player.Name = "Player";
+            this.Player.Size = new System.Drawing.Size(25, 25);
+            this.Player.TabIndex = 0;
+            this.Player.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(671, 544);
+            this.Controls.Add(this.Screen);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.Screen.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.Player)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel Screen;
+        private System.Windows.Forms.PictureBox Player;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- PTFGame/PTFGame/Form1.cs
+++ PTFGame/PTFGame/Form1.cs
@@ -12,9 +12,72 @@ namespace PTFGame
 {
     public partial class Form1 : Form
     {
+
+        bool right;
+        bool left;
+
+        bool jump;
+
+        int G = 30;
+        int force;
+
         public Form1()
         {
+
             InitializeComponent();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (right == true) { Player.Left += 5; }
+            if (left == true) { Player.Left -= 5; }
+
+            if (jump == true)
+            {
+                Player.Top -= force;
+                force -= 1;
+            }
+
+            if (Player.Top + Player.Height >= Screen.Height)
+            {
+                Player.Top = Screen.Height - Player.Height;
+                jump = false;
+            }
+            else
+            {
+                Player.Top += 5;
+            }
+      
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Right) { right = true; }
+            if (e.KeyCode == Keys.Left) { left = true; }
+            if (e.KeyCode == Keys.Escape) { this.Close(); }
+
+            if (jump != true)
+            {
+                if (e.KeyCode == Keys.Space)
+                {
+                    jump = true;
+                    force = G;
+                }
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Right) { right = false; }
+            if (e.KeyCode == Keys.Left) { left = false; }
+           
+
         }
     }
 }
