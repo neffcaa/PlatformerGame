0a6d4279b8802ceb023cc2d7c5f75d63640503f9
Zach <zacharywright77@gmail.com>
2016-11-01 22:38:38 -0500

    Collison with platform kinda?

--- PTFGame/PTFGame/Form1.Designer.cs
+++ PTFGame/PTFGame/Form1.Designer.cs
@@ -32,24 +32,27 @@
             this.components = new System.ComponentModel.Container();
             this.Screen = new System.Windows.Forms.Panel();
             this.movementTimer = new System.Windows.Forms.Timer(this.components);
-            this.badDude = new PTFGame.Enemy();
-            this.player = new PTFGame.Player();
             this.gravity = new System.Windows.Forms.Timer(this.components);
+            this.player = new PTFGame.Player();
+            this.badDude = new PTFGame.Enemy();
+            this.platform1 = new PTFGame.Platform();
             this.Screen.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.badDude)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.badDude)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.platform1)).BeginInit();
             this.SuspendLayout();
             // 
             // Screen
             // 
+            this.Screen.Controls.Add(this.platform1);
             this.Screen.Controls.Add(this.player);
             this.Screen.Controls.Add(this.badDude);
             this.Screen.Dock = System.Windows.Forms.DockStyle.Fill;
             this.Screen.Location = new System.Drawing.Point(0, 0);
+            this.Screen.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Screen.Name = "Screen";
-            this.Screen.Size = new System.Drawing.Size(671, 544);
+            this.Screen.Size = new System.Drawing.Size(1006, 837);
             this.Screen.TabIndex = 0;
-            
             // 
             // movementTimer
             // 
@@ -57,43 +60,56 @@
             this.movementTimer.Interval = 1;
             this.movementTimer.Tick += new System.EventHandler(this.timer1_Tick);
             // 
-            // badDude
-            // 
-            this.badDude.BackColor = System.Drawing.Color.Red;
-            this.badDude.Location = new System.Drawing.Point(450, 104);
-            this.badDude.Name = "badDude";
-            this.badDude.Size = new System.Drawing.Size(25, 25);
-            this.badDude.TabIndex = 1;
-            this.badDude.TabStop = false;
-            // 
-            // player
-            // 
-            this.player.BackColor = System.Drawing.Color.Blue;
-            this.player.Location = new System.Drawing.Point(325, 199);
-            this.player.Name = "player";
-            this.player.Size = new System.Drawing.Size(25, 25);
-            this.player.TabIndex = 2;
-            this.player.TabStop = false;
-            // 
             // gravity
             // 
             this.gravity.Enabled = true;
             this.gravity.Interval = 1;
             this.gravity.Tick += new System.EventHandler(this.gravity_Tick);
             // 
+            // player
+            // 
+            this.player.BackColor = System.Drawing.Color.Blue;
+            this.player.Location = new System.Drawing.Point(488, 306);
+            this.player.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(38, 38);
+            this.player.TabIndex = 2;
+            this.player.TabStop = false;
+            // 
+            // badDude
+            // 
+            this.badDude.BackColor = System.Drawing.Color.Red;
+            this.badDude.Location = new System.Drawing.Point(675, 160);
+            this.badDude.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.badDude.Name = "badDude";
+            this.badDude.Size = new System.Drawing.Size(38, 38);
+            this.badDude.TabIndex = 1;
+            this.badDude.TabStop = false;
+            // 
+            // platform1
+            // 
+            this.platform1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(52)))), ((int)(((byte)(52)))));
+            this.platform1.Location = new System.Drawing.Point(562, 612);
+            this.platform1.Name = "platform1";
+            this.platform1.Size = new System.Drawing.Size(318, 55);
+            this.platform1.TabIndex = 3;
+            this.platform1.TabStop = false;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(671, 544);
+            this.ClientSize = new System.Drawing.Size(1006, 837);
             this.Controls.Add(this.Screen);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
             this.Text = "Form1";
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
             this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
             this.Screen.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.badDude)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.badDude)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.platform1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -105,6 +121,7 @@
         private Enemy badDude;
         private Player player;
         private System.Windows.Forms.Timer gravity;
+        private Platform platform1;
     }
 }
 
---
+++ PTFGame/PTFGame/Platform.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PTFGame
+{
+    class Platform : GameObject
+    {
+        public override void collide(GameObject otherObject)
+        {
+            otherObject.jumpB = false;
+            otherObject.Top = this.Top - otherObject.Height;
+        }
+    }
+}
